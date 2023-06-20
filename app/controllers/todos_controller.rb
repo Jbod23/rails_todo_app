83fcee6f011dbbe1085cbd63fe6df589d0f1359e
Jbod23 <62573861+Jbod23@users.noreply.github.com>
2023-06-20 03:32:05 -0700

    Barebones todolist

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,43 @@
+class TodosController < ApplicationController
+        before_action :set_todo, only: [:edit, :update, :destroy]
+      
+        def index
+          @todos = Todo.all
+        end
+      
+        def new
+          @todo = Todo.new
+        end
+      
+        def create
+          @todo = Todo.new(todo_params)
+          if @todo.save
+            redirect_to todos_path, notice: 'Todo item was successfully created.'
+          else
+            render :new
+          end
+        end
+      
+        def update
+          if @todo.update(todo_params)
+            redirect_to todos_path, notice: 'Todo item was successfully updated.'
+          else
+            render :edit
+          end
+        end
+      
+        def destroy
+          @todo.destroy
+          redirect_to todos_path, notice: 'Todo item was successfully destroyed.'
+        end
+      
+        private
+          def set_todo
+            @todo = Todo.find(params[:id])
+          end
+      
+          def todo_params
+            params.require(:todo).permit(:title, :completed)
+          end
+end
+      
